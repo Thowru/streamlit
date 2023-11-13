a046c2b3384ec5585765c1f2b6ce5ab774695dfd
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-14 01:52:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn import preprocessing
-import pickle
+from datetime import datetime
 
-# 로그 데이터 처리 함수
+로그 데이터를 처리하는 함수
 def process_log_data(log_df):
     log_df.drop(columns='timestamp', inplace=True)
     log_df['Timestamp'] = log_df['message'].str.extract(r'(\d+/\w+/\d+\d+:\d+:\d+:\d+)')
@@ -22,44 +20,9 @@ def process_log_data(log_df):
     log_df = log_df[['Timestamp','Method','Protocol','Status','Referer','Path','Host','UA','Payload','Bytes']]
     return log_df
 
-# 특성 추출 함수
-def feature_extract(df):
-    df['method_cnt'] = df['Method'].nunique()
-    df['method_post'] = df['Method'].apply(lambda x: 1 if x == 'POST' else 0)
-    df['protocol_1_0'] = df['Protocol'].apply(lambda x: True if 'HTTP/1.0' in x else False)
-    df['status_major'] = df['Status'].apply(lambda x: 1 if x in ['200', '301', '302'] else 0)
-    df['status_404'] = df['Status'].apply(lambda x: 1 if x == '404' else 0)
-    df['status_499'] = df['Status'].apply(lambda x: True if x == '499' else False)
-    df['status_cnt'] = df['Status'].nunique()
-    df['path_same'] = df.groupby('Host')['Path'].transform(lambda x: x.value_counts().iloc[0] / len(x))
-    df['path_xmlrpc'] = df['Path'].apply(lambda x: 1 if 'xmlrpc.php' in x else 0)
-    df['ua_cnt'] = df.groupby('Host')['UA'].transform('nunique')
-    df['has_payload'] = df['Payload'].apply(lambda x: True if x != '-' else False)
-    df['bytes_avg'] = df.groupby('Host')['Bytes'].transform('mean')
-    df['bytes_std'] = df.groupby('Host')['Bytes'].transform('std')
-    return df
-
-# 이상 탐지 함수
-def anomaly_detection(df):
-    # 특성 선택
-    chosen_data = df[['method_cnt', 'method_post', 'protocol_1_0', 'status_major', 'status_404', 'status_499',
-                      'status_cnt', 'path_same', 'path_xmlrpc', 'ua_cnt', 'has_payload', 'bytes_avg', 'bytes_std']]
-
-    # Min-Max 스케일링
-    min_max_scaler = preprocessing.MinMaxScaler()
-    np_scaled = min_max_scaler.fit_transform(chosen_data)
-    chosen_data = pd.DataFrame(np_scaled, columns=chosen_data.columns)
-
-    # 클러스터링
-    cluster_model = KMeans(n_clusters=2)  # 예시로 2개의 클러스터 사용
-    df['cluster'] = cluster_model.fit_predict(chosen_data)
-
-    # 클러스터 결과 리턴
-    return df
-
-# Streamlit 앱
+Streamlit 앱
 def main():
-    st.title('로그 데이터 처리 및 이상 탐지 앱')
+    st.title('로그 데이터 처리 앱')
 
     # 파일 업로드
     uploaded_file = st.file_uploader("CSV 파일 선택", type="csv")
@@ -71,26 +34,17 @@ def main():
         # 로그 데이터 처리
         processed_log_df = process_log_data(log_df)
 
-        # 특성 추출
-        feature_df = feature_extract(processed_log_df)
-
-        # 이상 탐지
-        anomaly_df = anomaly_detection(feature_df)
-
         # 처리된 데이터 표시
         st.write("처리된 로그 데이터:")
-        st.write(anomaly_df)
+        st.write(processed_log_df)
 
         # 처리된 데이터를 새로운 CSV 파일로 저장
         processed_file_path = 'processed_file.csv'
-        anomaly_df.to_csv(processed_file_path, index=False)
+        processed_log_df.to_csv(processed_file_path, index=False)
 
         # 처리된 파일을 다운로드할 수 있는 링크 제공
         st.markdown(f"처리된 데이터 다운로드: [처리된 파일]({processed_file_path})")
 
-if __name__ == '__main__':
-    main()
 
-
-if __name__ == '__main__':
+if name == 'main':
     main()
