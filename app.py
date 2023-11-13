57433681c0b7dd04245a7baf8d3bae43d7d41f18
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-13 23:54:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -46,44 +46,6 @@ def main():
         # 처리된 파일을 다운로드할 수 있는 링크 제공
         st.markdown(f"처리된 데이터 다운로드: [처리된 파일]({processed_file_path})")
 
-from sklearn.ensemble import IsolationForest  # 이상탐지 모델 예시 (다른 모델로 대체 가능)
-from sklearn.model_selection import train_test_split
-from sklearn import linear_model, tree, neighbors
-from sklearn import preprocessing
-from sklearn.cluster import KMeans
-from sklearn.cluster import DBSCAN
-from sklearn.decomposition import PCA
-from joblib import dump, load
-
-# 전처리된 데이터 로드
-processed_file_path = 'train_processed.csv'
-processed_log_df = pd.read_csv(processed_file_path)
-
-# 이상탐지 모델 학습
-# 여기에서는 Isolation Forest를 사용하였습니다. 다른 이상탐지 모델을 사용할 수도 있습니다.
-model = IsolationForest(contamination=0.01)  # contamination은 이상치 비율을 나타냅니다.
-model.fit(processed_log_df[['method_cnt', 'method_post', 'status_major', 'status_404', 'bytes_avg', 'bytes_std']])
-
-# Streamlit 앱
-def main():
-    st.title('이상탐지 앱')
-
-    # 전처리된 데이터 표시
-    st.write("전처리된 로그 데이터:")
-    st.write(processed_log_df)
-
-    # 이상탐지 결과 예측
-    predictions = model.predict(processed_log_df[['method_cnt', 'method_post', 'status_major', 'status_404', 'bytes_avg', 'bytes_std']])
-    processed_log_df['anomaly'] = (predictions == -1)  # -1은 이상치를 나타냅니다.
-
-    # 이상치 시각화
-    st.write("이상치 여부:")
-    st.write(processed_log_df[['anomaly']])
-
-    # 이상치에 대한 상세 정보 표시
-    anomaly_details = processed_log_df[processed_log_df['anomaly']]
-    st.write("이상치 상세 정보:")
-    st.write(anomaly_details)
 
 if __name__ == '__main__':
     main()
